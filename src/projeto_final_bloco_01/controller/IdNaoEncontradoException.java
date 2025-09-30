e2e5a2436c3f2eeefc25662ec3fdad7e8ca51733
MyListon <myliston@gmail.com>
2025-09-30 12:50:49 -0300

    Correção no class IdNaoEncontradoException

--- src/projeto_final_bloco_01/controller/IdNaoEncontradoException.java
+++ src/projeto_final_bloco_01/controller/IdNaoEncontradoException.java
@@ -2,6 +2,11 @@ package projeto_final_bloco_01.controller;
 
 public class IdNaoEncontradoException extends RuntimeException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	// Construtor que recebe a mensagem de erro
 	public IdNaoEncontradoException(String mensagem) {
 		super(mensagem);
